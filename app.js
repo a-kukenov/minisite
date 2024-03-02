5e39a3cb812a559ff6f2df59f5cb19703208d69a
anti <kukenovpall@gmail.com>
2024-03-02 17:59:48 +0300

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express') // подключение express
+const path = require('path') // подключение path
+
+const server = express() // вызов express 
+
+const PORT = 3000
+
+// функция, определения пути до файла
+const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`)
+
+//базовая прослушка сервера
+server.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
+
+server.use(express.static(__dirname + '/pages'))
+//базовая имплементация роутинга на express
+server.get('/', (req, res) => {
+    res.sendFile(createPath('main', 'html'))
+})
+server.get('/calc', (req, res) => {
+    res.sendFile(createPath('calc', 'html'))
+})
+server.get('/settings', (req, res) => {
+    res.sendFile(createPath('settings', 'html'))
+})
+server.get('/getcss', (req, res) => {
+    res.sendFile(createPath('/styles/main', 'css'))
+})
+server.get('/redirect', (req, res) => {
+    res.redirect('/calc')
+})
+server.use((req, res) => {
+    res.status(404).sendFile(createPath('error'))
+})
